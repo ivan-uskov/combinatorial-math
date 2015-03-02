c48b34650274276221b244cc1d8d355052981fdf
Ivan Uskov <ivan.uskov@cpslabs.net>
2015-03-02 18:40:54 +0300

    Lw1

---
+++ lw1/exchange/stdafx.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+#include <vector>
+#include <string>
+#include <functional>
+#include <algorithm>
+#include <iterator>
+#include <memory>
+#include <exception>
+#include <map>
+#include <iostream>
